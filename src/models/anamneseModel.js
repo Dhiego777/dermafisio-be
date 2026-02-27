eb4c56ffc5600ef4f1a71908f5c71a8ab48e703d
Dhiego777 <dhiego-cavalcante@hotmail.com>
2026-02-27 18:34:35 -0300

    alteracao do models para receber todos os dados

--- src/models/anamneseModel.js
+++ src/models/anamneseModel.js
@@ -2,35 +2,35 @@ module.exports = class Anamnese {
   constructor(anamnese) {
     // Dados Pessoais
     this.nome = anamnese.nome;
-    this.data_nasc = anamnese.data_nasc;
+    this.data_nasc = anamnese.dataNasc;
     this.idade = anamnese.idade;
     this.endereco = anamnese.endereco;
     this.profissao = anamnese.profissao;
     this.telefone = anamnese.telefone;
-    this.como_conheceu = anamnese.como_conheceu;
+    this.como_conheceu = anamnese.comoConheceu;
 
     // Queixa Principal
-    this.queixa_principal = anamnese.queixa_principal;
+    this.queixa_principal = anamnese.queixaPrincipal;
 
     // Histórico Patológico
     this.medicacao = anamnese.medicacao;
     this.exames = anamnese.exames;
     this.alergia = anamnese.alergia || "nao";
-    this.alergia_qual = anamnese.alergia_qual;
+    this.alergia_qual = anamnese.alergiaQual;
     this.intestino = anamnese.intestino;
-    this.ciclo_menstrual = anamnese.ciclo_menstrual || "sim";
+    this.ciclo_menstrual = anamnese.cicloMenstrual || "sim";
     this.anticoncepcional = anamnese.anticoncepcional || "nao";
     this.hipotensao = anamnese.hipotensao || "nao";
-    this.doencas_pre = anamnese.doencas_pre || "nao";
-    this.doencas_pre_qual = anamnese.doencas_pre_qual;
+    this.doencas_pre = anamnese.doencasPre || "nao";
+    this.doencas_pre_qual = anamnese.doencasPreQual;
 
     // Histórico Social e Outros
-    this.atividade_fisica = anamnese.atividade_fisica;
+    this.atividade_fisica = anamnese.atividadeFisica;
     this.alimentacao = anamnese.alimentacao;
     this.liquido = anamnese.liquido;
     this.fuma = anamnese.fuma;
     this.alcool = anamnese.alcool;
-    this.procedimento_estetico = anamnese.procedimento_estetico;
+    this.procedimento_estetico = anamnese.procedimentoEstetico;
     this.cirurgia = anamnese.cirurgia;
     this.gravida = anamnese.gravida;
     this.metais = anamnese.metais;
